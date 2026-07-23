b7be925eb0f38b15ba36c2ff3df187658a703def
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-07-23 00:31:58 -0700

    added new features to remove bugs

---
+++ EntityFrameworkCore.Data/Migrations/20260723070154_CheckChanges.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Spain ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Spain");
+        }
+    }
+}
