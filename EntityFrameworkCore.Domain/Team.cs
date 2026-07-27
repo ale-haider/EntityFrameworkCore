6572d263e8f1d3f7bfa753945f6cd2255bffc383
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-07-27 00:38:32 -0700

    solved dmgl viewer problem

--- EntityFrameworkCore.Domain/Team.cs
+++ EntityFrameworkCore.Domain/Team.cs
@@ -4,11 +4,16 @@
     {
         //public int Id { get; set; } // inherits from BaseDomainMOdel
         public string? Name { get; set; }
+
+        public Coach Coach { get; set; }
         public int CoachId { get; set; }
 
         public int? LeagueId { get; set; }   
 
         public League? League { get; set; }
 
+
+        public List<Match> HomeMatches { get; set; } = new List<Match>() { };
+        public List<Match> AwayMatches { get; set; } = new List<Match>() { };
     }   
 }
