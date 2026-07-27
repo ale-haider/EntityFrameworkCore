fbbdfe554712cbf7a3f5023b6d2c0811fd978b56
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-07-26 23:24:36 -0700

    added 1 to many relationship between league and teams

--- EntityFrameworkCore.Domain/Team.cs
+++ EntityFrameworkCore.Domain/Team.cs
@@ -4,9 +4,11 @@
     {
         //public int Id { get; set; } // inherits from BaseDomainMOdel
         public string? Name { get; set; }
-
-        public int LeagueId { get; set; }   
-
         public int CoachId { get; set; }
+
+        public int? LeagueId { get; set; }   
+
+        public League? League { get; set; }
+
     }   
 }
